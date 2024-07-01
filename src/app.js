3a9e57df6f14ce4e322e20a2c2b02415d1d5d977
Olaleye Obidiya <dev.olaleye@smsabuja.com>
2024-07-01 21:34:06 +0100

    Vercel configuration

--- src/app.js
+++ src/app.js
@@ -1,30 +1,31 @@
 const express = require("express");
+const requestIp = require("request-ip");
 const { lookup } = require("geoip-lite");
-// const requestIp = require("request-ip");
 
 const app = express();
-// app.use(requestIp.mw());
-app.set("trust proxy", true);
 
 app.get("/api/hello", (req, res) => {
-  const ip = req.ip;
-  console.log(ip);
-  const geo = lookup(ip);
-  console.log(geo);
+  const clientIp = requestIp.getClientIp(req);
+  // const ip = req.ip;
+  console.log(clientIp);
+  const geo = lookup(clientIp);
+  // console.log(geo);
   if (req.query.visitor_name) {
     return res.send({
-      client_ip: ip,
+      client_ip: clientIp,
       location: geo.city,
-      greeting: `Hello, ${req.query.visitor_name}!, the temperature is 11 degrees Celsius in ${geo.city}`,
+      greeting: `Hello, ${req.query.visitor_name}!, the temperature is 11 degrees Celsius in New York`,
     });
   }
   return res.send({
-    client_ip: ip,
+    client_ip: clientIp,
     location: geo.city,
-    greeting: `Hello, Visitor!, the temperature is 11 degrees Celsius in ${geo.city}`,
+    greeting: `Hello, Visitor!, the temperature is 11 degrees Celsius in New York`,
   });
 });
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
+
+module.exports = app;
