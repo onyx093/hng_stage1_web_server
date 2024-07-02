d0fb03efb98e5f43791e1d6a7a2a0744491129c4
Olaleye Obidiya <dev.olaleye@smsabuja.com>
2024-07-02 05:02:39 +0100

    Minor bug fixes

--- src/app.js
+++ src/app.js
@@ -1,31 +1,40 @@
 const express = require("express");
-const requestIp = require("request-ip");
+const axios = require("axios");
 const { lookup } = require("geoip-lite");
 
 const app = express();
 
 app.get("/api/hello", (req, res) => {
-  const clientIp = requestIp.getClientIp(req);
-  // const ip = req.ip;
+  let clientIp = (
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    ""
+  )
+    .split(",")[0]
+    .trim();
+  clientIp = clientIp.split(":").pop();
   console.log(clientIp);
   const geo = lookup(clientIp);
-  // console.log(geo);
-  if (req.query.visitor_name) {
+  console.log(geo);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${geo.ll[0]}&lon=${geo.ll[1]}&units=metric&appid=d66e8772cf7b6a0d9c743b81e060063f`;
+
+  axios({ url }).then(({ data }) => {
+    console.log({ data });
+    if (req.query.visitor_name) {
+      return res.send({
+        client_ip: clientIp,
+        location: geo.city,
+        greeting: `Hello, ${req.query.visitor_name}!, the temperature is ${data.main.temp} degrees Celsius in ${geo.city}`,
+      });
+    }
     return res.send({
       client_ip: clientIp,
       location: geo.city,
-      greeting: `Hello, ${req.query.visitor_name}!, the temperature is 11 degrees Celsius in New York`,
+      greeting: `Hello, Visitor!, the temperature is ${data.main.temp} degrees Celsius in ${geo.city}`,
     });
-  }
-  return res.send({
-    client_ip: clientIp,
-    location: geo.city,
-    greeting: `Hello, Visitor!, the temperature is 11 degrees Celsius in New York`,
   });
 });
 
 app.listen(4000, () => {
   console.log("Server is running on port 4000");
 });
-
-module.exports = app;
